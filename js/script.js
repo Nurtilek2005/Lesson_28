f63fb49619c9a8d754963c86c60f617eb31c9b6e
kairat <ltdcvet@gmail.com>
2021-01-12 21:06:53 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -46,27 +46,75 @@
 // let currentCows = 7;
 // let currentChickens = 20;
 // let currentSheeps = 40;
-let animals = [
-  {name: 'cows', count: 7, max: 2},
-  {name: 'chickens', count: 20, max: 4},
-  {name: 'sheeps', count: 40, max: 3},
-]
+// let animals = [
+//   {name: 'cows', count: 7, max: 2},
+//   {name: 'chickens', count: 20, max: 4},
+//   {name: 'sheeps', count: 40, max: 3},
+//   {name: 'horses', count: 2, max: 2}
+// ]
+// let fruits = [
+//   {name: 'banana', count: 7, max: 2},
+//   {name: 'apple', count: 20, max: 2},
+// ]
 
-let printFarmInventory = function (units) {
-  for(let item of units) {
-    console.log(zeroFill(item.count, item.max,) + ' ' + item.name);
-  }
-  // console.log(zeroFill(cows, 3) + ' cows');
-  // console.log(zeroFill(chickens, 3) + ' chickens');
-  // console.log(zeroFill(sheeps, 3) + ' sheeps');
-};
+// let printFarmInventory = function (units) {
+//   for(let item of units) {
+//     console.log(zeroFill(item.count, item.max,) + ' ' + item.name);
+//   }
+//   // console.log(zeroFill(cows, 3) + ' cows');
+//   // console.log(zeroFill(chickens, 3) + ' chickens');
+//   // console.log(zeroFill(sheeps, 3) + ' sheeps');
+// };
 
-let zeroFill = function (number, width) {
-  let string = String(number);
-  while(string.length < width) {
-    string = "0" + string;
+// let zeroFill = function (number, width) {
+//   let string = String(number);
+//   while(string.length < width) {
+//     string = "0" + string;
+//   }
+//   return string;
+// }
+
+// printFarmInventory(animals);
+// console.log('==============================')
+// printFarmInventory(fruits);
+
+
+
+// while(true) {
+//   let number = parseInt( prompt('Введите любое число') )
+//   if(number >= 0) {
+//     alert('Вы ввели положительное число');
+//     break;
+//   } else if (number < 0){
+//     alert('Вы ввели отрицательное число');
+//     break;
+//   }
+//   if(number === null) {
+//     alert('Вы нажали "отмена"')
+//     break;
+//   }
+//   if(isNaN(number)) {
+//     alert('Укажите число!');
+//     break;
+//   }
+//}
+let number = parseInt( prompt('Введите число') );
+let validatePromptNumber = function (number) {
+  if(number === null) {
+    return 'Вы отменили ввод.';
   }
-  return string;
+  if(isNaN(number) || number === '') {
+    return 'Введите корректные данные!';
+  }
+  if(number > 0) {
+    return 'Вы ввели положительное число';
+  } else if(number < 0) {
+    return 'Вы ввели отрицательное число';
+  } else {
+    return 'Число равно 0';
+  }
 }
 
-printFarmInventory(animals);
+let result = validatePromptNumber(number);
+
+alert(result);
